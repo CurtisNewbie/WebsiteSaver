4c0ac9c527d6f29f31c676dadb00d2676ea0d077
CurtisNewbie <zhuangyongj@gmail.com>
2020-09-12 17:50:38 +0800

    Project init

---
+++ websiteSaver/src/main/java/com/curtisnewbie/WebsiteSaverApplication.java
@@ -0,0 +1,13 @@
+package com.curtisnewbie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteSaverApplication.class, args);
+	}
+
+}
