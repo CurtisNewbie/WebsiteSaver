83b2a55d0e7efe7e663105c4abc2777050d84512
CurtisNewbie <zhuangyongj@gmail.com>
2020-09-13 20:32:21 +0800

    Add boostrap log

--- websiteSaver/src/main/java/com/curtisnewbie/WebsiteSaverApplication.java
+++ websiteSaver/src/main/java/com/curtisnewbie/WebsiteSaverApplication.java
@@ -1,5 +1,7 @@
 package com.curtisnewbie;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebsiteSaverApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(WebsiteSaverApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(WebsiteSaverApplication.class, args);
+        logger.info(">>> Website Saver Application Running...");
     }
 
 }
