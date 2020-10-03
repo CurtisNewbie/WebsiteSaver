50fb2b5150938a3dde6dc8188e426410161524ba
CurtisNewbie <zhuangyongj@gmail.com>
2020-10-03 11:44:27 +0800

    Update javadoc

--- websiteSaver/src/main/java/com/curtisnewbie/api/RsaService.java
+++ websiteSaver/src/main/java/com/curtisnewbie/api/RsaService.java
@@ -12,9 +12,18 @@ import java.security.PublicKey;
  */
 public interface RsaService {
 
+    /**
+     * Get Public Key as a Base64 encoded string
+     */
     String getPublicKeyStr();
 
+    /**
+     * Get private key
+     */
     PrivateKey getPrivateKey();
 
+    /**
+     * Get public key
+     */
     PublicKey getPublicKey();
 }
