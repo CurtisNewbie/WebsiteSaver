600f0f9dc6d2ca0a6a1e05c1c9dc39933e672512
CurtisNewbie <zhuangyongj@gmail.com>
2021-02-27 11:38:03 +0800

    Self-signed SSL certificate

---
+++ websiteSaver/self-sign-ssl.md
@@ -0,0 +1,14 @@
+# Self-Signed Certificate
+
+Command using keytool that comes with jdk8
+
+```
+keytool -genkeypair -alias baeldung -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore baeldung.p12 -validity 3650
+
+```
+
+src: 
+
+```
+https://www.baeldung.com/spring-boot-https-self-signed-certificate
+```
---
+++ websiteSaver/src/main/java/com/curtisnewbie/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.curtisnewbie.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author yongjie.zhuang
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
+    }
+}
