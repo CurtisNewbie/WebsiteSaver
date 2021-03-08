737d285ea823474007052c0742caee5f23ed481d
CurtisNewbie <zhuangyongj@gmail.com>
2021-03-08 22:42:16 +0800

    Update build script

--- websiteSaver/build.sh
+++ websiteSaver/build.sh
@@ -1,16 +1,33 @@
-FRONTEND_ROOT=frontend/static
-JAVA_RESOUCES=src/main/resources/static
+frontendStatic=frontend/static
+javaStatic=src/main/resources/static
+buildLog="build.log"
+jarName="websiteSaver-0.0.1.jar"
 
-mkdir $JAVA_RESOUCES
+if [ -f $buildLog ]; then
+    rm $buildLog
+fi
+
+echo "Start building project, see '$buildLog'"
+
+if [ ! -d $javaStatic ]; then
+    echo "The 'static' folder (${javaStatic}) for java project not exists, creating it..." 
+    mkdir $javaStatic
+fi
 
 # copy hosted files
-cp -rv $FRONTEND_ROOT/.* $JAVA_RESOUCES
+echo "Copying frontend files to 'static' folder"
+cp -rv ${frontendStatic}/* $javaStatic >> $buildLog
 
 # do a clean package
-mvn clean package
+echo "Executing maven clean package"
+mvn clean package >> $buildLog
 
 # remove hosted files
-rm -rvf $JAVA_RESOUCES
+echo "Removing frontend files from 'static' folder"
+rm -rf $javaStatic/* >> $buildLog
 
-find . -name websiteSaver-*.jar -type f -exec cp {} . \;
+find target/ -name ${jarName} -type f -exec cp {} . \;
 
+if [ -f "${jarName}" ]; then
+    echo "Build success: '$(readlink -e ${jarName})'"
+fi
