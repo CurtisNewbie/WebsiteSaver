19e2b36c07ac2404c5329f98f57351552accd344
CurtisNewbie <zhuangyongj@gmail.com>
2021-03-31 00:24:35 +0800

    Fix mapper registration

--- websiteSaver/src/main/java/com/curtisnewbie/WebsiteSaverApplication.java
+++ websiteSaver/src/main/java/com/curtisnewbie/WebsiteSaverApplication.java
@@ -1,15 +1,15 @@
 package com.curtisnewbie;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 /**
  * @author zhuangyongj
  */
-@ImportResource(locations = "classpath:*/UserMapper.xml")
+@MapperScan(basePackages = "com.curtisnewbie.dao")
 @SpringBootApplication
 public class WebsiteSaverApplication {
 
--- websiteSaver/src/main/java/com/curtisnewbie/dao/UserMapper.java
+++ websiteSaver/src/main/java/com/curtisnewbie/dao/UserMapper.java
@@ -1,12 +1,10 @@
 package com.curtisnewbie.dao;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 /**
  * @author yongjie.zhuang
  */
-@Mapper
 public interface UserMapper {
 
     UserEntity findByUsername(@Param("username") String username);
